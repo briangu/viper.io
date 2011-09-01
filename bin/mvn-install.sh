31ee10c4486918380876a5f8dbc5f66bd2b99906
Brian Guarraci <brian.guarraci@gmail.com>
2011-08-31 20:41:46 -0700

    add javax.jms to make build work

--- bin/mvn-install.sh
+++ bin/mvn-install.sh
@@ -6,3 +6,6 @@ mvn install:install-file -Dfile=lib/jmxtools-1.2.1.jar -DgroupId=com.sun.jdmk -D
 #echo "installing kafka 0.0.6-SNAPSHOT"
 mvn install:install-file -Dfile=lib/kafka-0.7.jar -DgroupId=com.sna-projects.kafka -DartifactId=kafka -Dversion=0.7 -Dpackaging=jar
 
+mvn install:install-file -Dfile=lib/javax.jms-1.1.jar -DgroupId=jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/javax.jms-1.1.jar -DgroupId=javax.jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar
+
