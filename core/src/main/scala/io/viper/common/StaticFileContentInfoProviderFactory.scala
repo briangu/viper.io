abe21ad514b4338e54c99c753b456c11a1928b16
Brian Guarraci <bguarraci@linkedin.com>
2012-04-13 12:29:20 -0700

    make file and s3 server html kind of work

--- core/src/main/scala/io/viper/common/StaticFileContentInfoProviderFactory.scala
+++ core/src/main/scala/io/viper/common/StaticFileContentInfoProviderFactory.scala
@@ -6,7 +6,7 @@ import io.viper.core.server.file.{FileContentInfoProvider, InsertOnlyFileContent
 
 object StaticFileContentInfoProviderFactory
 {
-  var _enableCache = false;
+  var _enableCache = true;
 
   def enableCache(flag: Boolean) = _enableCache = flag
 
