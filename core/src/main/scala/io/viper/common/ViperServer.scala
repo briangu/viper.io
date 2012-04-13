766f967a7de1965ac67448e83de5c827ebea3361
Brian Guarraci <bguarraci@linkedin.com>
2012-04-13 09:57:42 -0700

    update viper server

--- core/src/main/scala/io/viper/common/ViperServer.scala
+++ core/src/main/scala/io/viper/common/ViperServer.scala
@@ -2,35 +2,21 @@ package io.viper.common
 
 
 import io.viper.core.server.file.StaticFileServerHandler
-import org.jboss.netty.channel.{DefaultChannelPipeline, ChannelPipeline, ChannelPipelineFactory}
-import collection.mutable.ArrayBuffer
-import io.viper.core.server.router._
-import java.util.Map
+import org.jboss.netty.channel.{ChannelPipeline, ChannelPipelineFactory}
 
 
 class ViperServer(resourcePath: String) extends ChannelPipelineFactory with RestServer
 {
-  override def getPipeline: ChannelPipeline =
-  {
-    import scala.collection.JavaConverters._
-    val routes = addDefaultRoutes
-    val lhPipeline: ChannelPipeline = new DefaultChannelPipeline
-    lhPipeline.addLast("uri-router", new RouterMatcherUpstreamHandler("uri-handlers", routes.toList.asJava))
-    return lhPipeline
+  override def getPipeline: ChannelPipeline = {
+    addDefaultRoutes
+    super.getPipeline
   }
 
   override def addRoutes = {}
 
-  private def addDefaultRoutes: ArrayBuffer[Route] =
-  {
-    val routes = ArrayBuffer[Route]()
-    routes.append(new GetRoute("/hello", new RouteHandler
-    {
-      def exec(args: Map[String, String]): RouteResponse = new Utf8Response("world")
-    }))
+  private def addDefaultRoutes {
     val provider = StaticFileContentInfoProviderFactory.create(this.getClass, resourcePath)
-    routes.append(new GetRoute("/$path", new StaticFileServerHandler(provider)))
-    routes.append(new GetRoute("/", new StaticFileServerHandler(provider)))
-    routes
+    get("/$path", new StaticFileServerHandler(provider))
+    get("/", new StaticFileServerHandler(provider))
   }
 }
